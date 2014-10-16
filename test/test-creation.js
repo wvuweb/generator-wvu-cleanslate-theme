97bd5a30588fc17d3c1bd3802d8bfd0c2d1d9b44
Steven Spriggs <steven.spriggs@gmail.com>
2014-10-16 14:28:12 -0400

    fix none passing build by passing in mock prompt data

--- test/test-creation.js
+++ test/test-creation.js
@@ -34,7 +34,7 @@ describe('wvu-cleanslate-theme generator', function () {
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': 'Y'
+      'author_name': 'John Doe'
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
